610f99236aa300952f693e013cc9bcfc44bbab87
Joao Pedro Pinheiro <jprnp18@gmail.com>
2023-05-18 23:49:00 -0300

    AOC 2022 Day 3 - pt1 - Rust

---
+++ src/aoc/aoc3/mod.rs
@@ -0,0 +1,38 @@
+use std::fs;
+
+pub fn rucksack_reorganization() {
+    let mut sum: usize = 0;
+    if let Ok(file) = fs::read_to_string("src/aoc/aoc3/input") {
+        file.split('\n').for_each(|line| {
+            if line.trim().len() > 0 {
+                sum += find_common_item_value(line);
+            }
+        });
+    } else {
+        panic!("File not found")
+    }
+
+    println!("{}", sum);
+}
+
+fn find_common_item_value(items: &str) -> usize {
+    let middle_idx = items.len() / 2;
+    for i in 0..middle_idx {
+        let x = items.chars().nth(i).unwrap();
+        for j in middle_idx..items.len() {
+            let y = items.chars().nth(j).unwrap();
+            if x == y {
+                return item_value(x);
+            }
+        }
+    }
+    return 0;
+}
+
+fn item_value(item: char) -> usize {
+    let ascii_val = item as u32;
+    if item.is_uppercase() {
+        return (ascii_val - 38) as usize;
+    }
+    return (ascii_val - 96) as usize;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::aoc::aoc1::count_calories;
 use crate::aoc::aoc2::jo_ken_po;
 use crate::aoc::aoc2::pt2::jo_ken_po_pt2;
+use crate::aoc::aoc3::rucksack_reorganization;
 
 mod aoc;
 
@@ -8,4 +9,5 @@ fn main() {
     count_calories();
     jo_ken_po();
     jo_ken_po_pt2();
+    rucksack_reorganization();
 }
